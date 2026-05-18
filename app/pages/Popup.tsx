c3e501da0a20cd102252e5f5b3a4ce58ee61660e
Ahmad Raza <ahmadrazababu96@gmail.com>
2026-05-18 20:35:23 +0500

    fixed nextjs router issue

--- app/pages/Popup.tsx
+++ app/pages/Popup.tsx
@@ -1,23 +1,29 @@
+"use client";
+
 import React from "react";
-import { useParams } from "react-router";
+import { useParams } from "next/navigation";
 import { initialProducts } from "../data/Data";
 
 const Popup = () => {
+  const params = useParams();
 
+  const product = initialProducts.find(
+    (item) => item.id.toString() === params.id
+  );
+
+  if (!product) {
+    return <h1>Product not found</h1>;
+  }
 
   return (
- 
-      
-            <img
-              src={product.image}
-              alt={product.name}
-
-              className='w-96 h-96  object-contain  rounded-2xl shadow-2xl 
-shadow-gray-950/50 border-4 border-gray-800'
-            />
-
- 
- )
-}
+    <div>
+      <img
+        src={product.image}
+        alt={product.name}
+        className="w-96 h-96 object-contain rounded-2xl"
+      />
+    </div>
+  );
+};
 
 export default Popup;
